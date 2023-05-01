1647fa9cd007426d781288cef36bb319b5e63cc4
balakarunanithi saravanapandian <balahockey8797@gmail.com>
2023-05-01 22:06:16 +0530

    Q1.java modified

--- basics/Q1.java
+++ basics/Q1.java
@@ -1,7 +1,26 @@
 package basics;
+
+import java.util.Scanner;
+
 class Q1{
+    static Scanner sc = new Scanner(System.in); 
 	public static void main(String[] args) {
-		System.out.println("Heloo 1");
-		System.out.println("Heloo 2");
+		System.out.println("Enter the number : ");
+		int n = sc.nextInt();
+		int start = 2,count =1,res = 1;
+	    
+		while(true) {
+			if(count == n)
+				break;
+			if(start == 2) {
+				res *=start;
+				start+=1;
+			}
+			res *=start;
+			start+=2;
+			count++;
+		}
+		System.out.println(n+" = "+res);
+		
 	}
 }
