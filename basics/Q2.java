a16069ff6b974dd3c9eb560bd5b66504e0516ba9
balakarunanithi saravanapandian <balahockey8797@gmail.com>
2023-05-02 22:00:24 +0530

    add a value plus 10

--- basics/Q2.java
+++ basics/Q2.java
@@ -2,7 +2,8 @@ package basics;
 class Q2{
 	public static void main(String[] args) {
 		int a = 10;
-		System.out.println("Heloo 1");
+		a+=10;
+		System.out.println("Heloo 1"+a);
 		System.out.println("This is bala's heaven");
 	}
 }
